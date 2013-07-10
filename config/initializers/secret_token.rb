00aa4053d01ec471b23c7f1430ce2c2c0b9172ef
Charlotte Cameron <charlottemidgley@gmail.com>
2013-07-10 14:11:32 +0100

    Initial commit for quiz_site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizSite::Application.config.secret_key_base = 'ed5194fbd9400ee52279d41571b23c325fe8175522e3455d3da90f758703181e7a44ec0b7c3f051dedcc4f33d6c778ca778687fa8234fcebc2f16f27be2924d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuizSite::Application.config.session_store :cookie_store, key: '_quiz_site_session'
